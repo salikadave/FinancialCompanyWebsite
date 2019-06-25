6e9f1014b939b9e264afe44adb6dd6e00063e2ff
Salika Dave <salikadave26@gmail.com>
2019-06-25 20:50:14 +0530

    Added smooth scrolling and fixed sticky navbar

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ function initMap() {
         map: map
     });
 }
+
+//For Sticky navbar
+window.addEventListener('scroll', function () {
+    if(this.scrollY >10){
+        document.querySelector('#navbar').style.opacity = 0.9;
+    }
+    else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// Code for Smooth scrolling
+
+$('#navbar a, .btn').on('click', function(e){
+    if(this.hash !== ''){
+        e.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 100
+        }, 800);
+    }
+});
